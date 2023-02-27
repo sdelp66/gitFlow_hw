0ab9fce473c1c38a4ba1eb9283dfada2bae49051
luisnarv <115510650+luisnarv@users.noreply.github.com>
2023-02-27 10:40:47 -0500

    1

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+let num = number;
+
+    if (numero % 2 === 0){
+    let binary = (num % 2).toString();
+    for (; num > 1; ) {
+        num = parseInt(num / 2);
+        binary =  (num % 2) + (binary);
+    
     }
+  return binary
+}
+  
+
+}
+
+
+
+
