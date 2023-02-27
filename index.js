4b64fcbf26ce3627b02fe07cb4ed1cd4c1cf4895
Edwin <ing.eliasincio123@gmail.com>
2023-02-27 12:09:40 -0500

    funcion cambiada

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    function laCajaDePandora(numero){
+        let num = number;
+        
+            if (numero % 2 === 0){
+            let binary = (num % 2).toString();
+            for (; num > 1; ) {
+                num = parseInt(num / 2);
+                binary =  (num % 2) + (binary);
+        
+            }
+          return binary
+        }
     }
